65ce5e5d8ff36952a7e09ccaedfacd3b9a40c3bd
Victor Huggo <victorhuggocontato42@gmail.com>
2023-08-15 03:57:48 -0300

    add solution BinarySearchList

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+    let l = 0;
+    let r = haystack.length;
+
+    do {
+        const m = Math.floor(l + (r - l) / 2);
+
+        if (haystack[m] == needle) {
+            return true;
+        } else if (haystack[m] > needle) {
+            r = m;
+        } else {
+            l = m + 1
+        }
+    } while (l < r);
+
+    return false
 }
